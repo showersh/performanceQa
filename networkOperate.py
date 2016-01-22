2464434863f9f9d71f32c589191bc5a8cc9b2261
shower20131215 <showersh@163.com>
2016-01-22 15:09:32 +0800

    1, create network api   2, paralleled execute program

---
+++ networkOperate.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding=utf-8
+import httplib2
+import json
+import time
+import datetime
+import uuid
+from random import Random
+
+def genetateToken():
+    """
+    """
+    try:
+        result_dict = {'functionName':'generateToken',\
+                     'delayTime':'',\
+                       'auth_token': '',\
+                   'isOK': False,\
+                    'statusCode':''
+                  }
+        h = httplib2.Http()
+        body = '{"auth":{"tenantName":"admin","passwordCredentials":{"username":"admin","password":"admin"}}}'
+        resp, content = h.request("http://192.168.122.37:5000/v2.0/tokens","POST",body,headers={'Content-Type':'application/json'})
+        #print type(content)
+        if(int(resp.status) == 200):
+        #print type(resp.status)
+            return_valuse = json.loads(content)
+            result_dict['auth_token'] = return_valuse['access']['token']['id']
+            result_dict['isOK'] = True
+    except Exception,e:
+        print e
+        result_dict['isOK'] = False
+    finally:
+        return result_dict
+
+def createNetwork(auth_token, network_name):
+    """
+    """
+    try:
+        result_dict = {'function_name':'generate_token',\
+                     'letency_time':'',\
+                     'is_ok': False,\
+                    'status_code':''
+                  }
+        print auth_token
+        h = httplib2.Http()
+        body_dict = {"network":{"name":"","admin_state_up":"false"}}
+        body_dict['network']['name'] = network_name
+        body_str = json.dumps(body_dict)
+        print body_str
+        print type(body_str)
+        headers = {'Content-Type':'application/json', 'X-Auth-Token':''} 
+        headers['X-Auth-Token'] = auth_token
+	#starttime = datetime.datetime.now()
+	#start_time = time.clock()
+	start_time = time.time()
+	#long running
+
+        resp, content = h.request("http://192.168.122.37:9696/v2.0/networks","POST",body_str,headers)
+	#endtime = datetime.datetime.now()
+	#end_time = time.clock()
+	end_time = time.time()
+	#print type(endtime - starttime)
+	#result_dict['letency_time'] = str((end_time - start_time).mico)
+	result_dict['letency_time'] = str(end_time - start_time)
+        result_dict['status_code'] = resp.status
+        #print ((endtime - starttime).microseconds)
+        print resp
+        print content
+        result_dict['is_ok'] = True
+        #if(int(resp.status) == 200):
+        ##print type(resp.status)
+        #    return_valuse = json.loads(content)
+        #    result_dict['auth_token'] = return_valuse['access']['token']['id']
+        #    result_dict['isOK'] = True
+    except Exception,e:
+        print e
+    finally:
+        return result_dict
+
+def random_str(randomlength=8):
+    str = ''
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(randomlength):
+        str+=chars[random.randint(0, length)]
+    return str
+
+
+if __name__== "__main__":
+    print "the start time is %s " % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f'))
+    start_maint = time.time()
+    result_dict = genetateToken()
+    #print result_dict
+    #print #createNetwork(result_dict['auth_token'],'test2')
+    if result_dict['isOK'] :
+        task_name = uuid.uuid1()
+        for i in range(0,10):
+            #time.sleep(1)
+            network_name = random_str(8)
+            result_dict2 = createNetwork(result_dict['auth_token'],network_name)
+            with open("perfromqa.txt", 'a+') as f:
+                if( int(result_dict2['status_code']) == 201 or int(result_dict2['status_code']) == 201 ):
+                    information = '%s %s %s OK %s seconds\r\n' % (task_name,network_name, result_dict2['status_code'],result_dict2['letency_time'])
+                    #print information
+                else:
+                   
+                   information = '%s %s %s BAD %s seconds\r\n' % (task_name,network_name, result_dict2['status_code'],result_dict2['letency_time'])    
+                   print information
+                f.write(information)
+        end_maint = time.time()
+        with open("perfromqa.txt", 'a+') as f:
+            information = 'the task %s spend %s seconds\r\n' % (task_name,(end_maint - start_maint))
+            #print information
+            f.write(information)
+
+    print "the end time is %s " % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f'))
+
