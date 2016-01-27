acec3461e9e1d3a13f927a3f21dccd84552c25d1
shower20131215 <showersh@163.com>
2016-01-27 14:08:44 +0800

    case 1 and case 2 finished

--- generateData.py
+++ generateData.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import uuid
+import httplib2
+import json
 from random import Random
 
 class generateData(object):
@@ -8,7 +10,7 @@ class generateData(object):
     def __init__(self):
 
         self.task_name = ''
-        self.task_length = 3
+        self.task_length = 10
         self.concurrent_number = 1 
 
     def setCreateNetworkTaskName(self):
@@ -29,6 +31,13 @@ class generateData(object):
         """ genetate create Network task data
         """
         task_dict_list = []
+        authen_token = ''
+        result_dict = self.genetateToken()
+        if result_dict['isOK'] :
+            authen_token = result_dict['auth_token']
+        else:
+            print "fail generate authen tolen"
+            return []
 
         for i in range(0, self.task_length):
             task_dict = {'process_id' : '',\
@@ -37,14 +46,41 @@ class generateData(object):
                          'status_code' : '',\
                          'latency_time' : '',\
                          'concurrent_number' : '',\
+                         'auth_token' : '',\
             }
             task_dict['task_name'] = self.task_name
             task_dict['network_name'] = self.random_str()
             task_dict['concurrent_number'] = self.concurrent_number
+            task_dict['auth_token'] = authen_token
             task_dict_list.append(task_dict)
         return task_dict_list
 
+    def genetateToken(self):
+        """login and   generate tolen 
+        """
+        try:
+            result_dict = {'functionName':'generateToken',\
+                         'delayTime':'',\
+                           'auth_token': '',\
+                       'isOK': False,\
+                        'statusCode':''
+                      }
+            h = httplib2.Http()
+            body = '{"auth":{"tenantName":"admin","passwordCredentials":{"username":"admin","password":"admin"}}}'
+            resp, content = h.request("http://192.168.122.37:5000/v2.0/tokens","POST",body,headers={'Content-Type':'application/json'})
+            #print type(content)
+            if(int(resp.status) == 200):
+            #print type(resp.status)
+                return_valuse = json.loads(content)
+                result_dict['auth_token'] = return_valuse['access']['token']['id']
+                result_dict['isOK'] = True
+        except Exception,e:
+            print e
+            result_dict['isOK'] = False
+        finally:
+            return result_dict
 if __name__ == "__main__":
     object_network = generateData()
     object_network.setCreateNetworkTaskName()
     print object_network.generateNetworkTask()
+
