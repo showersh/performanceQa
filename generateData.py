16da281725ed3f1af827a2fc3866ed2e3a18e491
shower20131215 <showersh@163.com>
2016-01-29 11:23:34 +0800

    finish task list

--- generateData.py
+++ generateData.py
@@ -5,12 +5,13 @@ import json
 from random import Random
 
 class generateData(object):
-    """#genrate the data"""
+    """#genrate the task list  data """
 
     def __init__(self):
 
         self.task_name = ''
-        self.task_length = 10
+        self.current_task_name = ''
+        self.task_length = 100
         self.auth_token = ""
         self.network_task_list = []
         self.subnet_task_list = []
@@ -19,6 +20,12 @@ class generateData(object):
 
     def setCreateNetworkTaskName(self):
         self.task_name = "network_%s" % ( str(uuid.uuid1()))
+
+    def setCreateSubnetTaskName(self):
+        self.task_name = "subnet_%s" % ( str(uuid.uuid1()))
+
+    def setCreatePortTaskName(self):
+        self.task_name = "port_%s" % ( str(uuid.uuid1()))
 
     def random_str(self,randomlength=8):
         """ generate random arbitrary length string 
@@ -64,9 +71,17 @@ class generateData(object):
         """
         task_dict_list = []
         authen_token = ''
+        #self.task_length = task_length
         result_dict = self.genetateToken()
         if result_dict['isOK'] :
             authen_token = result_dict['auth_token']
+            network_name = self.random_str()
+            result_dict1 = self.generateNetworkId(authen_token, network_name)
+            if result_dict1['is_ok'] :
+                self.network_id = result_dict1['network_id']
+            else:
+                print "fail generate network_id"
+                return []
         else:
             print "fail generate authen tolen"
             return []
@@ -79,9 +94,11 @@ class generateData(object):
                          'latency_time' : '',\
                          'concurrent_number' : '',\
                          'auth_token' : '',\
+                         'network_id' : '',\
             }
             task_dict['task_name'] = self.task_name
             task_dict['network_name'] = self.random_str()
+            task_dict['network_id'] = self.network_id
             task_dict['concurrent_number'] = self.concurrent_number
             task_dict['auth_token'] = authen_token
             task_dict_list.append(task_dict)
@@ -92,9 +109,17 @@ class generateData(object):
         """
         task_dict_list = []
         authen_token = ''
+        #self.task_length = task_length
         result_dict = self.genetateToken()
         if result_dict['isOK'] :
             authen_token = result_dict['auth_token']
+            network_name = self.random_str()
+            result_dict1 = self.generateNetworkId(authen_token, network_name)
+            if result_dict1['is_ok'] :
+                self.network_id = result_dict1['network_id']
+            else:
+                print "fail generate network_id"
+                return []
         else:
             print "fail generate authen tolen"
             return []
@@ -102,16 +127,18 @@ class generateData(object):
         for i in range(0, self.task_length):
             task_dict = {'process_id' : '',\
                          'task_name' : '',\
-                         'network_name' : '',\
+                         'port_name' : '',\
                          'status_code' : '',\
                          'latency_time' : '',\
                          'concurrent_number' : '',\
                          'auth_token' : '',\
+                         'network_id' : '',\
             }
             task_dict['task_name'] = self.task_name
-            task_dict['network_name'] = self.random_str()
+            task_dict['port_name'] = self.random_str()
             task_dict['concurrent_number'] = self.concurrent_number
             task_dict['auth_token'] = authen_token
+            task_dict['network_id'] = self.network_id
             task_dict_list.append(task_dict)
         return task_dict_list
 
@@ -139,8 +166,55 @@ class generateData(object):
             result_dict['isOK'] = False
         finally:
             return result_dict
-if __name__ == "__main__":
-    object_network = generateData()
-    object_network.setCreateNetworkTaskName()
-    print object_network.generateNetworkTask()
 
+    def generateNetworkId(self, auth_token, network_name):
+        """generate a new network id  
+        """
+        try:
+            result_dict = {'function_name':'generateNetworkId',\
+                           'network_id':'',\
+                           'network_name':'',\
+                           'is_ok': False,\
+                           'status_code':''
+                          }
+            #print auth_token
+            h = httplib2.Http()
+            body_dict = {"network":{"name":"","admin_state_up":"false"}}
+            body_dict['network']['name'] = network_name
+            body_str = json.dumps(body_dict)
+            #print body_str
+            #print type(body_str)
+            headers = {'Content-Type':'application/json', 'X-Auth-Token':''} 
+            headers['X-Auth-Token'] = auth_token
+            resp, content = h.request('http://192.168.122.37:9696/v2.0/networks','POST',body_str,headers)
+            result_dict['status_code'] = resp.status
+            #print resp
+            #print type(resp)
+            #print content
+            #print type(content)
+            result_dict['is_ok'] = True
+            if(int(resp.status) == 200 or int(resp.status) == 201 ):
+                #print type(resp.status)
+                return_value = json.loads(content)
+                #print return_value
+                result_dict['network_id'] = return_value['network']['id']
+                result_dict['network_name'] = network_name
+                result_dict['is_ok'] = True
+
+        except Exception,e:
+            print "fail generate network id"
+            print e
+        finally:
+            return result_dict
+
+if __name__ == "__main__":
+    #object_network = generateData()
+    #object_network.setCreateNetworkTaskName()
+    #print object_network.generateNetworkTask()
+    #object_subnet = generateData()
+    #object_subnet.setCreateSubnetTaskName()
+    #print object_subnet.generateSubnetTask(1,1)
+    object_port = generateData()
+    object_port.setCreatePortTaskName()
+    print object_port.generatePortTask(1,1)
+    
